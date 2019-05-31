6cbd12aeadbf46c0961c028b7ea6c6a5e2b91240
Yosef Lehrer <yolehrer@gmail.com>
2019-05-31 09:35:21 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -5,8 +5,6 @@ end
 
 ## Comment the below back in to see method scope in action!
 
-#def rescue_princess_peach(status)
-#puts "#{status}"
-#end
-rescue_princess_peach
-puts status
+def rescue_princess_peach(status)
+puts "#{status}"
+end
